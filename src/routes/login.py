402db4574bfbeb245231ee233feaccc16812b9b8
guiijoo <gui.marchetto@hotmail.com>
2024-05-19 15:42:54 -0300

    sistema login finalizado inicio do sistem de bloqueio por não esta logado

--- src/routes/login.py
+++ src/routes/login.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from werkzeug.security import check_password_hash
 
-from flask_login import login_user
+from flask_login import login_user, login_required, logout_user
 from database.Models.users import Users
 
 login_route = Blueprint('login',__name__)
@@ -9,6 +9,8 @@ login_route = Blueprint('login',__name__)
 @login_route.route('/')
 def login_form():
     return render_template('login/login.html')
+
+
 
 @login_route.route('/auth', methods=['POST'])
 def login_auth():
@@ -20,5 +22,14 @@ def login_auth():
     if user and check_password_hash(user.user_password, password):
         login_user(user)
         return redirect(url_for('filmes.index'))
+    
+    flash('dados invalidos')
+    return render_template('login/login.html')
 
-    return 'dados invalidos'
+
+
+@login_route.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login/login.html'))
