c7988d1ab3608446019946b86ef26385c20b86ad
Paulo Ricardo Lopes Dionizio <66482705+Paulo-Ricardo-Lopes-Dionizio@users.noreply.github.com>
2024-05-21 22:15:39 -0300

    update

--- src/config.py
+++ src/config.py
@@ -1,16 +1,26 @@
 from routes.filmes import filme_route
+from routes.users import user_route
+from routes.login import login_route
 
 from database.database import db
 from database.Models.filmes import Filmes
+from database.Models.users import Users
+from datetime import timedelta
 
+import URI
 
 def configure_all(app):
     configure_db()
     configure_routes(app)
+    app.config['SECRET_KEY'] = URI.PASS_SECRET_KEY
+    app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=10)
+    # configure_googleOAuth(app)
 
 def configure_db():
     db.connect()
-    db.create_tables([Filmes])
+    db.create_tables([Filmes, Users])
 
 def configure_routes(app):
     app.register_blueprint(filme_route,url_prefix='/filmes')
+    app.register_blueprint(user_route,url_prefix='/user')
+    app.register_blueprint(login_route)
--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_login import LoginManager
 from config import *
 
 app = Flask(__name__)
@@ -8,5 +9,12 @@ def main():
     return 'teste'
 
 configure_all(app)
+
+login_manager= LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def user_loader(user_id):
+    return Users.get_by_id(user_id)
 
 app.run(debug=True)
